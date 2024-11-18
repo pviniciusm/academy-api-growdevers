d52200964565d2077fa804ecd91af0b876ccdff7
Paulo <pvinics@gmail.com>
2024-11-18 13:17:22 -0300

    post

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import * as dotenv from 'dotenv';
 import { growdevers } from './dados.js';
+import { randomUUID } from 'crypto';
 
 dotenv.config();
 
@@ -15,6 +16,29 @@ app.get("/growdevers", (req, res) => {
         mensagem: "Growdevers listados com sucesso",
         dados: growdevers
     });
+});
+
+app.post("/growdevers", (req, res) => {
+    // 1- entrada
+    const body = req.body;
+
+    const novoGrowdever = {
+        id: randomUUID(),
+        nome: body.nome,
+        email: body.email,
+        idade: body.idade,
+        matriculado: body.matriculado
+    }
+
+    // 2- processamento
+    growdevers.push(novoGrowdever);
+
+    // 3- saida
+    res.status(201).send({
+        ok: true,
+        mensagem: "Growdever criado com sucesso",
+        dados: growdevers
+    })
 });
 
 const porta = process.env.PORT;
