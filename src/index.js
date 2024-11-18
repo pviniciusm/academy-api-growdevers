9020559f09335c14136985d3118f42828cdb3ea1
Paulo <pvinics@gmail.com>
2024-11-17 23:58:49 -0300

    express project init

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-console.log("Hello world!");
+import express from 'express';
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+
+// Criar nossas rotas
+
+const porta = process.env.PORT;
+
+app.listen(porta, () => {
+    console.log("O servidor está executando na porta " + porta);
+});
